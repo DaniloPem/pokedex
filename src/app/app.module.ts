9cb6c7eae2fab38966726de09cc091ecf2bc1886
Danilo Pezo <danilo_97pem@hotmail.com>
2022-09-15 21:03:02 -0300

    Criado componente pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { PokemonsModule } from './componentes/pokemons/pokemons.module';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonsComponent } from './componentes/pokemons/pokemons.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PokemonsModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
